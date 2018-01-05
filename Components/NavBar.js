2de03f7a3b1493dae8e8d319550b42b74e8d3f87
Черкашин Александр Иванович <cherkashin1996@outlook.com>
2018-01-05 11:34:18 +0300

    Initial commit

---
+++ Components/NavBar.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+import { TabNavigator } from 'react-navigation'; // 1.0.0-beta.14
+import Ionicons from 'react-native-vector-icons/Ionicons'; // 4.4.2
+
+const HomeScreen = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Home Screen</Text>
+  </View>
+);
+
+const ProfileScreen = () => (
+  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <Text>Profile Screen</Text>
+  </View>
+);
+
+const NavBar = TabNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Ionicons
+          name={focused ? 'ios-home' : 'ios-home-outline'}
+          size={26}
+          style={{ color: tintColor }}
+        />
+      ),
+    },
+  },
+  Profile: {
+    screen: ProfileScreen,
+    navigationOptions: {
+      tabBarIcon: ({ tintColor, focused }) => (
+        <Ionicons
+          name={focused ? 'ios-person' : 'ios-person-outline'}
+          size={26}
+          style={{ color: tintColor }}
+        />
+      ),
+    },
+  },
+}, {
+    tabBarPosition: 'bottom',
+    tabBarOptions: {
+        showIcon: true,
+        showLabel: false,
+    }
+});
+
+export default NavBar;
